e166bae33f60d508878a8e4cb01f6a022cf6b85e
Tima-Ryabina <timofey.ryab@yandex.ru>
2025-05-18 17:59:43 +0800

    Первое окно

--- my_app.py
+++ my_app.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
 from instr import *
+from second_win import TestWin
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
